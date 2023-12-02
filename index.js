c2bd5fa7ecbd215b334a3916c844f57af5c0f0bf
Artem Dragunov <dragunovartem99@gmail.com>
2023-12-02 17:57:36 +0300

    Configure views path

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const port = 9999;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views")); // Run index.js outside of root dir
 
 app.get("/", (req, res) => {
 	res.render("home");

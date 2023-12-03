6167f9326b1203cc7d57894a263b6091844031e8
Artem Dragunov <dragunovartem99@gmail.com>
2023-12-03 17:58:48 +0300

    Add public dir

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 9999;
+const PORT = 9999;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views")); // Run index.js outside of root dir
@@ -11,6 +13,18 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+app.get("/random", (req, res) => {
+	const randomNumber = Math.floor(Math.random() * 10 + 1);
+	res.render("random", {
+		randomNumber,
+	});
+});
+
+app.get("/loop", (req, res) => {
+	const items = [1, 2, 3, 4, 5];
+	res.render("loop", { items });
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port http://localhost:${PORT}`);
 });
